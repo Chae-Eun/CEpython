3b193ca38ecf914dbe6ba192ccdfd6e612d8a1a6
Chae-Eun <ddangkong245@gmail.com>
2015-09-02 13:46:24 +0900

    [Lec01]150902
    문자열 예제

---
+++ project150902/project150902/project150902.py
@@ -0,0 +1,40 @@
+﻿#print("Here are the numbers!"+\
+#    "the first is {0:d} The second is {0:4d}" .format(7,8))
+
+#print("한글")
+#print('hickory dickorydock!\nthemouse ran up the clock')
+#print("="*10)
+
+#salary=int(input("Please enter your salary:"))
+#bonus=int(input("Please enter your bonus:"))
+
+#salary=input("Please enter your salary:")
+#bonus=input("Please enter your bonus:")
+#payCheck=salary+bonus
+#payCheck=float(salary)+float(bonus)
+
+#round(payCheck, 2)
+#print(payCheck)
+#print(type(payCheck))
+
+#name="greenjoa"
+#print(name[0])
+#print(name[-1])
+#print(name[-3])
+
+#info = "201311239greenjoa"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid + " " + sname)
+
+#a = "I eat %-10s apples." % "five"
+#print(a)
+#b = "Error is %d%%." %98
+#print(b)
+
+#a=","
+#a.join('abcd')
+#print(a)
+
+exit=input("종료할까요? ")
+print(exit.upper())
